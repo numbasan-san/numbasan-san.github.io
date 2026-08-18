b888aef3b5ab816aee2bc7b5139662de1b7ef16f
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2026-08-18 16:43:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// sw.js
+const CACHE_NAME = 'biblia-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
